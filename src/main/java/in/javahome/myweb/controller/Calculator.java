18b62e9cb5742cd9db03b5bfb9faecd36ffd034f
amerikefang <amerikefang@gmail.com>
2019-09-08 17:04:08 -0700

    add method of devide

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -12,11 +12,18 @@ public class Calculator {
 	public int add(int i, int j){
 		return i+j;
 	}
-	public int multiply(int i, int j){
-		return i*j;
-	}
 	
 	public int sub(int i, int j){
 		return i-j;
 	}
+	
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int devide(int i, int j){
+		return i/j;
+	}
+
+
 }
