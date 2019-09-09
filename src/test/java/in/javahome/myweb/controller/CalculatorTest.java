7c6072a0cdcef484bf0b20c03052e0579ab9a177
amerikefang <amerikefang@gmail.com>
2019-09-08 17:16:45 -0700

    udpate errors

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -9,7 +9,7 @@ public class CalculatorTest extends TestCase {
 		Assert.assertEquals(cal.add(10, 20), 30);
 	}
 	public void testsub(){
-		Assert.assertEquls(cal.sub(10,20),-10);
+		Assert.assertEquals(cal.sub(10,20),-10);
 	}
 	public void testMultiply(){
 		Assert.assertEquals(cal.multiply(10, 20), 200);
